c83414c8e0dac89ae5d54fdb5fefe7fafbea6a74
Rafael Farias <rafaelfariasprogramador@gmail.com>
2025-06-18 14:20:38 -0300

    Controller de Descrição da nota e prioridade

--- backend/src/controllers/PriorityController.js
+++ backend/src/controllers/PriorityController.js
@@ -1,7 +1,17 @@
 const Annotations = require("../models/AnnotationData");
 
 module.exports = {
-    read(request, response) {
+    async read(request, response) {
+        const priority = request.query;
+        const annotations = await Annotations.find(priority);
+        return response.json(annotations)
+    },
 
+    async update(request, response) {
+        const { id } = request.params;
+        const annotation = await Annotations.findOne({ _id: id });
+        annotation.priority = !annotation.priority;
+        await annotation.save();
+        return response.json(annotation);
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const AnnotationController = require("./controllers/AnnotationController");
 const PriorityController = require("./controllers/PriorityController");
+const ContentController = require('./controllers/ContentController');
 
 // Rotas para as anotações
 routes.get("/annotations", AnnotationController.read);
@@ -11,5 +12,9 @@ routes.delete("/annotations/:id", AnnotationController.delete);
 
 // Rotas para prioridades
 routes.get("/priorities", PriorityController.read);
+routes.post("/priorities/:id", PriorityController.update);
+
+// Rotas para notas
+routes.post("/content/:id", ContentController.update);
 
 module.exports = routes;
